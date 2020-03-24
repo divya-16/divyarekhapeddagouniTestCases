6337dda32a9ee2e2bcf993431eba192283e36b99
divya-16 <18wh5a0515@bvrithyderabad.edu.in>
2020-03-24 19:11:02 +0530

    Committed.

---
+++ tdd/src/main/java/epam/com/tdd/App.java
@@ -0,0 +1,13 @@
+package epam.com.tdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tdd/src/main/java/epam/com/tdd/RemoveFirst2Chars.java
@@ -0,0 +1,26 @@
+package epam.com.tdd;
+
+public class RemoveFirst2Chars {
+	public static String RemoveA(String str){
+		int len = str.length();
+		String res = "";
+		if(len == 0) {
+			res = "";
+		} else if(len == 1) {
+			if(str.charAt(0) != 'A') {
+				res = res + str.charAt(0);
+			}
+		} else {
+
+			if(str.charAt(0) != 'A') {
+				res = res + str.charAt(0);
+			} if(str.charAt(1) != 'A') {
+				res = res + str.charAt(1);
+			}
+			if(len > 2) {
+				res = res + str.substring(2);
+			}
+		}
+		return res;
+	}
+}
